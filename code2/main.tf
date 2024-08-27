e80dbe741e8ea036ebd1fda3c4f6a2340c6fc681
DonBarry <barryellow4life@gmail.com>
2024-08-27 17:44:23 -0500

    terraform code

---
+++ code2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_group" "developers" {
+  name = "developers2"
+
+}
+
+resource "aws_iam_user" "lb" {
+  name = "barry2026"
+}
---
+++ code3/output.tf
@@ -0,0 +1,13 @@
+output "instance_ip" {
+  value = aws_lightsail_instance.custom.public_ip_address
+}
+
+output "my_arn" {
+    value = aws_lightsail_instance.custom.arn
+  
+}
+
+output "user_name" {
+    value = aws_lightsail_instance.custom.username
+  
+}
